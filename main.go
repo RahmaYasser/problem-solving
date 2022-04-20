e7b627b60e500b2d818906893bd6292bc79815fe
RahmaYasser <rahma.y9079@gmail.com>
2022-04-20 17:53:07 +0200

    greedy,easy,can-place-flowers,v2

--- main.go
+++ main.go
@@ -1,34 +1,11 @@
 package main
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    length := len(flowerbed)
-    possible:=0
-    if length>1{
-        if flowerbed[0]==0 && flowerbed[1]==0{
-            flowerbed[0] = 1
-            possible++
-        }
-        if flowerbed[length-1]==0 && flowerbed[length-2]==0{
-            flowerbed[length-1] = 1
-            possible++
-        }
-        for i:=1;i<length-1;i++{
-            if flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0{
-                flowerbed[i]=1
-                possible++
-            }
-        } 
-    } else if length==1{
-        if flowerbed[0]==0{
-            possible++
-        }
-    }
-    if n > possible {
-        return false
-    }
-    return true
+	for i := 0; i < len(flowerbed) && n > 0; i++ {
+		if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+			n, i = n-1, i+1
+		}
+	}
+	return n == 0
 }
 
-
-func main(){
-
-}
+func main(){}
