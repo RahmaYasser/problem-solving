5a821392f1911391021c50d37f175f84c0c6a97b
RahmaYasser <rahma.y9079@gmail.com>
2022-04-22 14:27:47 +0200

    greedy,easy, valid palindrome

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
-func canPlaceFlowers(flowerbed []int, n int) bool {
-	for i := 0; i < len(flowerbed) && n > 0; i++ {
-		if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
-			n, i = n-1, i+1
-		}
-	}
-	return n == 0
+
+import "fmt"
+
+func validPalindrome(s string) bool {
+    if IsPalindrome(s) { return true }
+	l := 0;
+    h := len(s)-1;
+
+	
+    for(h > l){
+        if(s[l] != s[h]){
+			sSlice:=[]rune(s)
+			sSlice2:=[]rune(s)
+			newS:=append(sSlice[0:l], sSlice[l+1:]...)
+			if IsPalindrome(string(newS)){ return true} else { 
+				newS2:=append(sSlice2[0:h], sSlice2[h+1:]...)
+				if IsPalindrome(string(newS2)){ return true} else {
+					return false
+				}
+			}
+        }
+		l++
+		h--
+    }
+	return false
 }
 
-func main(){}
+func IsPalindrome(s string) bool {
+	l := 0;
+    h := len(s)-1;
+
+    for(h > l){
+        if(s[l] != s[h]){
+			return false
+        }
+		l++
+		h--
+    }
+	return true
+}
+func main(){
+	fmt.Println(validPalindrome("abxaxcba"))
+}
