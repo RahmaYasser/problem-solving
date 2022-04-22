81233d7068fc8ecd0248842c8f4c0498e62ec580
RahmaYasser <rahma.y9079@gmail.com>
2022-04-22 16:35:22 +0200

    greedy,easy,convertTime

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func lemonadeChange(bills []int) bool {
-	c5:=0
-	c10:=0
-    for _,cust:=range bills{
-		if cust==5{
-			c5++
-		}else if cust==10 {
-			if c5==0{ return false}
-			c10++
-			c5--
-		} else{
-			if c5==0{ return false}
-			if c10>0{
-				c10--
-				c5--
-			}else {
-				c5-=3
-			}
-		}
-		if c5<0 || c10<0 {
-			return false
-		}
-	}
-    return true
+func convertTime(current string, correct string) int {
+	ans:=0
+    currentH,_:=strconv.Atoi(current[:2])
+    correctH,_:=strconv.Atoi(correct[:2])
+
+	currentM,_:=strconv.Atoi(current[3:])
+    correctM,_:=strconv.Atoi(correct[3:])
+
+	minut:=correctH*60+correctM - (currentH*60+currentM)
+
+	ans += minut/60
+	minut = minut%60
+	ans += minut/15
+	minut = minut%15
+	ans += minut/5
+	minut = minut%5
+	ans += minut
+	return ans
 }
 func main(){
-	x := []int{10}
-	fmt.Println(lemonadeChange(x))
+	
+	fmt.Println(convertTime("08:35","11:00"))
 }
