bff7eb15544c7a6b458da5416cd438b46ad24446
RahmaYasser <rahma.y9079@gmail.com>
2022-07-29 21:27:54 +0200

    data structures study-plan, HashMap

--- main.go
+++ main.go
@@ -1,88 +1,42 @@
 package main
 
-/*
-1 1 1 2 2 3 3 3 3 3
-*/
-func merge(intervals [][]int) [][]int {
-	var res [][]int
-	quickSort(0, len(intervals)-1, intervals)
-	iIndex := 0
-	res = append(res, []int{intervals[0][0], intervals[0][1]})
-	for i := 1; i < len(intervals); i++ {
-		overlapX, overlapY := overlap(res[iIndex][0], res[iIndex][1], intervals[i][0], intervals[i][1])
-		if overlapX != -1 {
-			res[iIndex][0] = overlapX
-			res[iIndex][1] = overlapY
-		} else {
-			iIndex++
-			res = append(res, []int{intervals[i][0], intervals[i][1]})
-		}
-	}
-	return res
-}
-func overlap(x1 int, y1 int, x2 int, y2 int) (int, int) {
-	// there is no max.int in go :)
-	var y int
-	if y2 > y1 {
-		y = y2
-	} else {
-		y = y1
-	}
-	if x2 >= x1 && x2 <= y1 {
-		return x1, y
-	}
-	return -1, -1
-}
-func quickSort(low int, high int, arr [][]int) {
-	if low < high {
-		pivotIndex := partition(low, high, arr)
-		quickSort(low, pivotIndex-1, arr)
-		quickSort(pivotIndex+1, high, arr)
-	}
-}
-func partition(low int, high int, arr [][]int) int {
-	pivot := arr[high][0]
-	i := low - 1
-	for j := low; j < high; j++ {
-		if arr[j][0] <= pivot {
-			i++
-			arr[i][0], arr[j][0] = arr[j][0], arr[i][0]
-			arr[i][1], arr[j][1] = arr[j][1], arr[i][1]
-		}
-	}
-	//swap(i+1, high, arr)
-	arr[i+1][0], arr[high][0] = arr[high][0], arr[i+1][0]
-	arr[i+1][1], arr[high][1] = arr[high][1], arr[i+1][1]
-	return i + 1
+import "fmt"
+
+type MyHashMap struct {
+	m []int
 }
 
-/*
-res		num
-0		2
-2		2
-0		5
-5		4		0101  01000 -> 1101
-9		4
-13		8
+func Constructor() MyHashMap {
+	return MyHashMap{
+		m: make([]int, 1000005),
+	}
+}
 
+func (this *MyHashMap) Put(key int, value int) {
+	this.m[key] = value + 1
+}
 
-0011
-0101
-----
-0110
-0011
-----
-0101
-0101
-----
-0000
-0001
-----
-0001
-0011
-----
-0010
-0011
-----
-0001
-*/
+func (this *MyHashMap) Get(key int) int {
+	if this.m[key] == 0 {
+		return -1
+	}
+	return this.m[key] - 1
+}
+
+func (this *MyHashMap) Remove(key int) {
+	this.m[key] = 0
+}
+func main() {
+	obj := Constructor()
+	obj.Put(10, 20)
+	obj.Put(0, 20)
+	obj.Put(5, 20)
+	obj.Put(7, 20)
+	param_2 := obj.Get(10)
+	fmt.Println(param_2)
+	obj.Remove(10)
+	obj.Remove(0)
+	obj.Remove(5)
+	obj.Remove(7)
+	fmt.Println(obj.m)
+}
