188742543a8da9b55df43cbdd1180b50e747ab09
RahmaYasser <rahma.y9079@gmail.com>
2022-08-10 17:44:04 +0200

    data structures study-plan, Search 2D matrix

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
-/*func getRow(rowIndex int) []int {
-	var firstRow []int
-	firstRow = append(firstRow, 1)
-	for i := 1; i <= rowIndex; i++ {
-		var secondRow []int
-		secondRow = append(secondRow, 1)
-		for j := 1; j < len(firstRow); j++ {
-			secondRow = append(secondRow, firstRow[j]+firstRow[j-1])
-		}
-		secondRow = append(secondRow, 1)
-		firstRow = secondRow
-	}
-	return firstRow
-}
-*/
-func getRow(rowIndex int) []int {
-	result := make([]int, rowIndex+1)
-	result[0] = 1
-	for i := 1; i <= rowIndex; i++ {
-		for j := i; j > 0; j-- {
-			result[j] = result[j] + result[j-1]
+func searchMatrix(matrix [][]int, target int) bool {
+	m := len(matrix)
+	n := len(matrix[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == target {
+				return true
+			}
 		}
 	}
-
-	return result
+	return false
 }
 
 func main() {
-	getRow(3)
+
 }
