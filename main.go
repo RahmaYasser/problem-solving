da604217abd1e36c1e3d2a843c5581ce0d586bb9
RahmaYasser <rahma.y9079@gmail.com>
2022-08-12 11:24:34 +0200

    study plan, subarraySum

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
-func productExceptSelf(nums []int) []int {
-	size := len(nums)
-	prefix := 1
-	ans := make([]int, size)
-	for i := 0; i < size; i++ {
-		ans[i] = prefix
-		prefix *= nums[i]
-	}
-	prefix = 1
-	for i := size - 1; i >= 0; i-- {
-		ans[i] = prefix
-		prefix *= nums[i]
+func subarraySum(nums []int, k int) int {
+	n := len(nums)
+	ans := 0
+	for i := 0; i < n; i++ {
+		sum := 0
+		for j := i; j < n; j++ {
+			sum += nums[j]
+			if sum == k {
+				ans++
+			}
+		}
 	}
 	return ans
 }
 func main() {
-	nums := []int{1, 2, 3, 4}
-	productExceptSelf(nums)
+	nums := []int{1}
+	println(subarraySum(nums, 0))
 }
