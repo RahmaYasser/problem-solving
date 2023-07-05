dc704945d4d7303ef0f615eb3a3a22a502d14347
RahmaYasser <rahma.y9079@gmail.com>
2023-07-05 17:36:08 +0200

    running sum

--- main.cpp
+++ main.cpp
@@ -2,21 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); ++i){
-            nums[i] += nums[i - 1];
-        }
-        vector<int> ans;
-        for (auto query : queries) {
-            int index = upper_bound(nums.begin(), nums.end(), query) - nums.begin();
-            ans.push_back(index);
-        }
-        return ans;
-    }
-};
+vector<int> runningSum(vector<int>& nums) {
+    for(int i=1;i<nums.size();i++)nums[i]+=nums[i-1];
+    return nums;
+}
 int main() {
     cout <<"SOLVING ON LEETCODE";
     return 0;
