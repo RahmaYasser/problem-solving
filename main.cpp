ac03402ff4fb0e93b9422475a02b8a39b8c3d06f
RahmaYasser <rahma.y9079@gmail.com>
2023-07-05 18:00:58 +0200

    find-the-highest-altitude

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> runningSum(vector<int>& nums) {
-    for(int i=1;i<nums.size();i++)nums[i]+=nums[i-1];
-    return nums;
+int largestAltitude(vector<int>& gain) {
+    int sum=0,max=0;
+    for(auto g:gain){
+        sum+=g;
+        if(sum>max)max=sum;
+    }
+    return max;
 }
 int main() {
     cout <<"SOLVING ON LEETCODE";
