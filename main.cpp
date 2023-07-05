e23bc4abc8e6bf9da3b6263c8bbddeacdfab9abf
RahmaYasser <rahma.y9079@gmail.com>
2023-07-05 19:47:29 +0200

    left-and-right-sum-differences

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pivotInteger(int n) {
-    int sum = n*(n+1)/2;
-    for(int i=1;i<=n;i++){
-        if(i*(i+1)/2==sum-((i-1)*i)/2)return i;
+vector<int> leftRightDifference(vector<int>& nums) {
+    int l=nums.size();
+    int prefixSum[l];
+    vector<int>answer(l);
+    prefixSum[0]=nums[0];
+    for(int i=1;i<l;i++){
+        prefixSum[i]=nums[i]+prefixSum[i-1];
     }
-    return -1;
+    answer[0] = abs(prefixSum[l-1]-prefixSum[0]);
+    for(int i=1;i<l;i++){
+        answer[i] = abs(prefixSum[i-1]-(prefixSum[l-1]-prefixSum[i]));
+    }
+    return answer;
 }
 int main() {
     cout <<"SOLVING ON LEETCODE";
