49768c5c576fa22ec2c095bed9df92233c3821d5
RahmaYasser <rahma.y9079@gmail.com>
2023-07-05 17:27:44 +0200

    longest-subsequence-with-limited-sum

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findMiddleIndex(vector<int>& nums) {
-    vector<int> prefixSum(nums.size()+1,0);
-    for(int i=1;i<prefixSum.size();i++){
-        prefixSum[i] = nums[i-1]+prefixSum[i-1];
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i){
+            nums[i] += nums[i - 1];
+        }
+        vector<int> ans;
+        for (auto query : queries) {
+            int index = upper_bound(nums.begin(), nums.end(), query) - nums.begin();
+            ans.push_back(index);
+        }
+        return ans;
     }
-    //if(prefixSum[0] == prefixSum[prefixSum.size()-1]-prefixSum[1])return 0;
-    for(int i=1;i<prefixSum.size();i++){
-        if(prefixSum[i-1]==prefixSum[prefixSum.size()-1]-prefixSum[i])return i-1;
-    }
-    return -1;
-}
+};
 int main() {
     cout <<"SOLVING ON LEETCODE";
     return 0;
