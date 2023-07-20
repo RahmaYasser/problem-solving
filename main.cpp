4bbf9499878ec00c77c172f67c5c27f6079bdfdc
RahmaYasser <rahma.y9079@gmail.com>
2023-07-20 16:03:38 +0200

    Continuous Subarray Sum

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ bool checkSubarraySum(vector<int>& nums, int k) {
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             if (!mp.count(sum % k))
+
                 mp[sum % k] = i + 1;
             else if (mp[sum % k] < i)
                 return true;
