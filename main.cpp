ace958a9459280bb5c80221987312eb3bf0324e1
RahmaYasser <rahma.y9079@gmail.com>
2023-09-26 21:51:50 +0200

    primes

--- main.cpp
+++ main.cpp
@@ -3,96 +3,47 @@
 typedef long long ll;
 using namespace std;
 
-
-// PRIMES
-bool IsPrime(long long n){
-    bool tPrime=false;
-    for(int i=2;i*i<=n;i++){
-        if(i*i==n){
-            n/=i;
-            tPrime= true;
-            break;
-        }
+int binarySearch(int target,vector<int>&primes){
+    int st=0,end=78499,mid;
+    while(st<end){
+        mid=st+(end-st+1)/2;
+        if(primes[mid]==target)return mid;
+        if(primes[mid]>target)end=mid-1;
+        else st=mid;
     }
-    if(!tPrime)return false;
-    if(n%2==0||n==1)return n==2;
-    for(int i=3;i*i<=n;i+=2){
-        if(n%i==0)return false;
-    }
-    return true;
-}
-
-// DIVISORS
-vector<int> findDivisor(int n){
-    vector<int> v;
-    for(int i=1;i*i<=n;i++){
-        if(n%i==0){
-            v.push_back(i);
-            if(i!=n/i)v.push_back(n/i);
-        }
-    }
-    return v;
-}
-
-// PRIMES FACTORS
-vector<int> findPrimeFactor(int n){
-    vector<int> v;
-    while(n%2==0){
-        n/=2;
-        v.push_back(2);
-    }
-    for(int i=3;i*i<=n;i+=2){
-        while(n%i==0){
-            n/=i;
-            v.push_back(i);
-        }
-    }
-    // what if this number is prime?
-    if(n>1)v.push_back(n);
-    for(auto x:v)cout <<x<<" ";
-    return v;
-}
-
-int countPrimes(int n){
-    if (n==0)return 0;
-    vector<bool>isPrime(n,true);
-    isPrime[0]=isPrime[1]=false;
-    for(int i=2;i*i<=n;i++){
-        if(isPrime[i]){
-            for(int y=i+i; y<n;y+=i){
-                isPrime[y]=false;
-            }
-        }
-    }
-    int count=0;
-    for(auto x:isPrime)if(x)count++;
-    return count;
+    return end;
 }
 int main(){
     int MAX_N=1000009;
-
-    vector<bool> isPrime(MAX_N,true);
+    int n,t;
+    cin >>t;
+    vector<bool>isPrime(MAX_N,true);
+    vector<int>primes;
     isPrime[0]=isPrime[1]=false;
     for(int i=2;i*i<=MAX_N;i++){
         if(isPrime[i]){
-            for(int y=i+i; y<=MAX_N;y+=i){
+            for(int y=i+i;y<MAX_N;y+=i){
                 isPrime[y]=false;
             }
         }
     }
-    vector<ll>tPrimesList;
-    for(ll i=2;i<isPrime.size();i++){
-        if(isPrime[i]){
-            tPrimesList.push_back(i*i);
-        }
+    for(int i=2;i<isPrime.size();i++){
+        if(isPrime[i])primes.push_back(i);
     }
-    int n;cin>>n;
-    ll x;
-    while(n--){
-        cin>>x;
-        // binary search
-        if(binary_search(tPrimesList.begin(),tPrimesList.end(),x))cout<<"YES\n";
-        else cout <<"NO\n";
+    vector<ll>prefixSum(primes.size());
+    prefixSum[0]=2;
+    for(int i=1;i<primes.size();i++){
+        prefixSum[i]=primes[i]+prefixSum[i-1];
+    }
+    while(t--) {
+        cin >> n;
+        if(n==1)cout <<0;
+        else{
+            n= binarySearch(n,primes);
+            cout <<prefixSum[n]<<"\n";
+        }
     }
     return 0;
 }
+
+
