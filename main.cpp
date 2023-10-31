7df984491f621a5948ee943e201c25bd7f574a49
RahmaYasser <rahma.y9079@gmail.com>
2023-10-31 15:37:11 +0200

    count max level of nodes for each connected component

--- main.cpp
+++ main.cpp
@@ -4,45 +4,44 @@ typedef long long ll;
 using namespace std;
 
 /*
- * count max level of nodes for each connected component
- * return max count
+ * count number of connected components
  */
 
-int bfs(vector<vector<int>>&adj,int start){
-    int level=0,current,size;
+void bfs(vector<vector<int>>&adj,vector<bool>&visited,int start){
+    visited[start]=true;
+    int current;
     queue<int>q;
     q.push(start);
     while(!q.empty()){
-        size=q.size();
-        while(size--){
-            current=q.front();
-            q.pop();
-            for(int i : adj[current]){
+        current=q.front();
+        q.pop();
+        for(int i : adj[current]){
+            if(!visited[i]){
+                visited[i]=true;
                 q.push(i);
             }
         }
-        level++;
     }
-    return level;
 }
 
 int main(){
-    int n,x;
+    int n,node,count=0;
     cin>>n;
+    vector<bool>visited(n+1);
     vector<vector<int>> adj(n+1);
-    vector<int>startingPoints;
-    for(int i=1;i<=n;i++){
-        cin >> x;
-        if(x==-1)startingPoints.push_back(i);
-        else adj[x].push_back(i);
-    }
-    int mxLevel=-1,lvl;
-    for(auto s:startingPoints){
-        lvl=bfs(adj,s);
-        mxLevel=max(mxLevel,lvl);
-    }
-    cout <<mxLevel<<"\n";
 
+    for(int i=1;i<=n;i++){
+        cin >> node;
+        adj[i].push_back(node);
+        adj[node].push_back(i);
+    }
+    for(int i=1;i<=n;i++){
+        if(!visited[i]){
+            count++;
+            bfs(adj,visited,i);
+        }
+    }
+    cout <<count;
 
     return 0;
 }
