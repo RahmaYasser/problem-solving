9939f4115ff1dfcd466425eced96dc82c817e866
RahmaYasser <rahma.y9079@gmail.com>
2023-10-31 14:59:54 +0200

    count max level of nodes for each connected component

--- main.cpp
+++ main.cpp
@@ -2,62 +2,47 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 using namespace std;
+
 /*
- * Is bipartite graph?
- * 1. color the src node
- * 2. traverse to children to mark with opposite color
- * 3. if any child is already colored with the same color as parent, return false
- * 4. return true if all graph is traversed
- * 5. note that, call bfs for each connected component
+ * count max level of nodes for each connected component
+ * return max count
  */
 
-
-
-bool isBipartite(vector<vector<int>>&adj,vector<int>&colors,int start){
-    queue<int> q;
+int bfs(vector<vector<int>>&adj,int start){
+    int level=0,current,size;
+    queue<int>q;
     q.push(start);
-    colors[start]=1;
-    int current,child;
     while(!q.empty()){
-        current=q.front();
-        q.pop();
-        for(int i=0;i<adj[current].size();i++){
-            child=adj[current][i];
-            if(colors[child]==0){
-                colors[child]=-colors[current];
-                q.push(child);
-            }
-            else{
-                if(colors[child]==colors[current])return false;
+        size=q.size();
+        while(size--){
+            current=q.front();
+            q.pop();
+            for(int i : adj[current]){
+                q.push(i);
             }
         }
+        level++;
     }
-    return true;
+    return level;
 }
+
 int main(){
-    int t,n,e,x,y;
-    cin>>t;
-    for(int o=1;o<=t;o++){
-        cin>>n>>e;
-        vector<vector<int>>adj(n+1);
-        vector<int>colors(n+1,0);// not colored
-        for(int i=0;i<e;i++){
-            cin>>x>>y;
-            adj[x].push_back(y);
-            adj[y].push_back(x);
-        }
-        cout <<"Scenario #"<<o<<":\n";
-        bool bipartite=true;
-        for(int i=1;i<=n;i++){
-            if(colors[i]==0){
-                if(!isBipartite(adj,colors,i)){
-                    bipartite=false;
-                    break;
-                }
-            }
-        }
-        if(bipartite)cout <<"No suspicious bugs found!\n" ;
-        else cout <<"Suspicious bugs found!\n";
+    int n,x;
+    cin>>n;
+    vector<vector<int>> adj(n+1);
+    vector<int>startingPoints;
+    for(int i=1;i<=n;i++){
+        cin >> x;
+        if(x==-1)startingPoints.push_back(i);
+        else adj[x].push_back(i);
     }
+    int mxLevel=-1,lvl;
+    for(auto s:startingPoints){
+        lvl=bfs(adj,s);
+        mxLevel=max(mxLevel,lvl);
+    }
+    cout <<mxLevel<<"\n";
+
+
     return 0;
 }
