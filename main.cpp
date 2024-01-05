ef814b319e559dc6a9a8a223880abcfebec097b3
RahmaYasser <rahma.y9079@gmail.com>
2024-01-05 20:05:44 +0200

    disjoint set union O(n)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// spoj, ada and indexing!
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,72 +5,28 @@ using namespace std;
 #define FOR(i,st,end) for(int i=st;i<=end;i++)
 typedef long long ll;
 
-const int alphabetCount = 26;
-class TrieNode{
-public:
-    TrieNode(){
-        for(int i=0;i<alphabetCount;i++){
-            nodes[i]= nullptr;
-        }
-        isWord=0;
-    }
-    TrieNode* nodes[alphabetCount]{};
-    int isWord;
-    ~TrieNode(){
-        for(auto i : nodes){
-            delete i;
-        }
-    }
-};
+const int N=100;
+int pr[N];
 
-class Trie{
-private: TrieNode* root;
-public:
-
-    Trie(){
-        root = new TrieNode;
-    }
-    void insert(const string& s){
-        TrieNode* cur = root;
-        for(auto c : s){
-            if(cur->nodes[c - 'a'] == nullptr){
-                cur->nodes[c - 'a'] = new TrieNode;
-            }
-            cur = cur->nodes[c - 'a'];
-            cur->isWord++;
-        }
-    }
-    int search( string& s){
-        TrieNode* cur = root;
-        for(auto c : s){
-            if(cur->nodes[c - 'a'] == nullptr){
-                return 0;
-            }
-            cur = cur->nodes[c - 'a'];
-        }
-        //cur is pointing to the nodes of the last character
-        return cur->isWord;
-    }
-    ~Trie(){
-        delete root;
-    }
-};
-int main() {
-    int n,k;cin >>n>>k;
-    string s;
-    Trie *trie = new Trie;
-    FOR(i,1,n){
-        cin >>s;
-        trie->insert(s);
-    }
-    int ans;
-    FOR(i,1,k){
-        cin >>s;
-        ans = trie->search(s);
-        cout <<ans<<"\n";
-    }
-    return 0;
+void init(){
+    iota(pr, pr+N,0);
+}
+int parent(int node){
+    if(node==pr[node])return node;
+    return parent(pr[node]);
+}
+void connect(int node1,int node2){
+    node1=parent(node1);
+    node2=parent(node2);
+    if(node1==node2) return;
+    pr[node1]=node2; // node1 is the child
 }
 
+bool connected(int node1, int node2){
+    return parent(node1)== parent(node2);
+}
 
+int main() {
 
+    return 0;
+}
