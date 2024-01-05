c574807cf054552cf6b66eb8097ec7636e5fa4fe
RahmaYasser <rahma.y9079@gmail.com>
2024-01-05 20:09:09 +0200

    disjoint set union O(logn)

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ typedef long long ll;
 
 const int N=100;
 int pr[N];
+int sz[N];
 
 void init(){
     iota(pr, pr+N,0);
+    iota(sz,sz+N,1);
 }
 int parent(int node){
     if(node==pr[node])return node;
@@ -19,7 +21,11 @@ void connect(int node1,int node2){
     node1=parent(node1);
     node2=parent(node2);
     if(node1==node2) return;
-    pr[node1]=node2; // node1 is the child
+    if(sz[node2] > sz[node1]){
+        swap(node1,node2);
+    }
+    sz[node1] += sz[node2];
+    pr[node2]=node1; // node2 is the child
 }
 
 bool connected(int node1, int node2){
