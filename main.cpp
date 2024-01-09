b4a55f600739443a0830ba4b97b5df0aa0fb208f
RahmaYasser <rahma.y9079@gmail.com>
2024-01-09 09:43:32 +0200

    dsu problems

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Codeforces 939D
+// Codeforces 277A
 
 
 #include <bits/stdc++.h>
@@ -10,17 +10,18 @@ typedef long long ll;
 
 
 class DSU{
-public:int N=26;
+public:int N;
 
-    explicit DSU() {
-        pr=vector<int>(N);
+    explicit DSU(int n):N(n){
+        pr=vector<int>(N,-1);
         sz=vector<int>(N,1);
-        iota(pr.begin(), pr.end(),0);
+       // iota(pr.begin(), pr.end(),0);
     }
 
 public:vector<int> pr;
 public:vector<int> sz;
-private:int parent(int node){
+public:int parent(int node){
+        if(pr[node]==-1)return -1;
         if(node==pr[node])return node;
         return pr[node]=parent(pr[node]);
     }
@@ -43,22 +44,46 @@ public:bool connected(int node1, int node2){
 
 
 int main() {
-    int n;
-    cin >>n;
-    DSU* dsu = new DSU();
-    string s1,s2;
-    cin >>s1>>s2;
-    vector<pair<char,char>> res;
-    for(int i=0;i<s1.size();++i){
-        if(!dsu->connected(s1[i]-'a',s2[i]-'a')){
-            dsu->connect(s1[i]-'a',s2[i]-'a');
-            res.emplace_back(s1[i],s2[i]);
+    int n,m,x,node1,node2,ans=0;
+    cin >>n >>m;
+    m++;
+    DSU* dsu = new DSU(m);
+    for(int i=0;i<n;++i){
+        cin >> x;
+        if(x){
+            cin >> node1;
+            if(dsu->pr[node1]==-1)dsu->pr[node1] = node1;
+            x--;
+        }else ans++;
+        while (x--){
+            cin >> node2;
+            if(dsu->pr[node2]==-1)dsu->pr[node2] = node2;
+            dsu->connect(node1,node2);
+            node1=node2;
         }
     }
-    cout <<res.size()<<"\n";
-    for(auto p: res){
-        cout <<p.first <<" "<<p.second<<"\n";
+    int i=1;
+    while(i<m){
+        x=dsu->parent(i);
+        i++;
     }
 
+    sort(dsu->pr.begin(),dsu->pr.end());
+    node2=0;
+    x=m;
+    for(int i=1;i<m;i++){
+        if(dsu->pr[i]!=-1){
+            node1 = dsu->pr[i];
+            x=i;
+            break;
+        }
+    }
+    for(int i=x+1;i<m;i++){
+        if(dsu->pr[i]!=node1){
+            node1=dsu->pr[i];
+            ans++;
+        }
+    }
+    cout <<ans;
     return 0;
 }
