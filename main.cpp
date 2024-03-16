dbcc8defc866e65af2fb0e594cd7b9133689e916
RahmaYasser <rahma.y9079@gmail.com>
2024-03-16 17:08:13 +0200

    sparce table v1

--- main.cpp
+++ main.cpp
@@ -1,62 +1,52 @@
+// A - Potentiometers
 #include <bits/stdc++.h>
+
 using namespace std;
+using ll = long long;
 
-const int N = 1e5 + 5, P = 17;
 
-int n, a[N], LOG[N];
 
-void prepare() {
-    for (int i = 2; i < N; ++i)
-        LOG[i] = LOG[i / 2] + 1;
-}
 
-struct {
-    int st[P][N];
-    int operation(int lf, int rt) {
-        return min(lf, rt);
+class SparseTable{
+    static const int N = 1e5 + 5,P=17;
+    public:int a[N];
+    public: int table[P][N];
+    private:static int operation(int a, int b){
+        return min(a,b);
     }
-    void build() {
-        for (int i = 0; i < n; ++i)
-            st[0][i] = a[i];
-        for (int p = 1; (1 << p) <= n; ++p) {
-            for (int i = 0; i + (1 << p) <= n; ++i) {
-                int x = st[p - 1][i];
-                int y = st[p - 1][i + (1 << (p - 1))];
-                st[p][i] = operation(x, y);
+public:void build (int n){
+        for(int i=0;i<n;i++){
+            table[0][i] = a[i];
+        }
+        for(int p=1;(1<<p) <= n;p++){
+            for(int i=0;i+(1<<p)-1 < n;i++){
+                table[p][i] = operation(table[p-1][i], table[p-1][i+(1<<(p-1))]);
             }
         }
     }
-    int operator()(int l, int r) {
-        int p = LOG[r - l + 1];
-        int x = st[p][l];
-        int y = st[p][r - (1 << p) + 1];
-        return operation(x, y);
+public:int query(int l,int r){
+        int ans = 1e9;
+        for(int p=P;p>=0;p--){
+            if(r-l+1 >= (1<<p)){
+                ans = operation(ans,table[p][l]);
+                l+=(1<<p);
+            }
+        }
+        return ans;
     }
-} st;
+};
+
 
 int main() {
-    //ios::sync_with_stdio(false); cout.tie(nullptr); cin.tie(nullptr);
-    prepare();
-    int k,mx=-INT_MAX;
-    cin >> n>>k;
-    for (int i = 0; i < n; ++i){
-        cin >> a[i];
-        mx = max(mx,a[i]);
+    int n,l,r; cin >>n;
+    SparseTable *st=new SparseTable;
+    for(int i=0;i<n;i++)cin >> st->a[i];
+    st->build(n);
+    int q;cin>>q;
+    for(int i=0;i<q;i++){
+        cin >>l>>r;
+        cout <<st->query(l,r)<<"\n";
     }
-    st.build();
-    int mini = st(0,n-1);
-    if(k==1)cout <<mini;
-    else if(k==2){
-        int ans=-INT_MAX;
-        int l=0,r=0;
-        while (r<n-1){
-            mx = max(st(l,r),st(r+1,n-1));
-            ans = max(ans,mx);
-            ++r;
-        }
-        cout <<ans;
-    }
-    else cout << mx;
     return 0;
 }
 
