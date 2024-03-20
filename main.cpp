f150a964f3c95509454ee9d25420ccecb107c197
RahmaYasser <rahma.y9079@gmail.com>
2024-03-20 15:38:50 +0200

    sparse table advanced

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
-// A - Potentiometers
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 
-
-
-
 class SparseTable{
     static const int N = 1e5 + 5,P=17;
-    public:int a[N];
-    public: int table[P][N];
+public:int a[N];
+public: int table[P][N];
 private: int log[N];
 
-    private:static int operation(int a, int b){
-        return min(a,b);
+private:static int operation(int a, int b){
+        return max(a,b);
     }
 public:void build (int n){
         for(int i=0;i<n;i++){
@@ -33,21 +29,25 @@ public:void build (int n){
     }
 public:int query(int l,int r){
         int p = log[r-l+1];
-        return min(table[p][l], table[p][r - (1<<p) + 1]);
+        return operation(table[p][l], table[p][r - (1<<p) + 1]);
     }
 };
 
 
 int main() {
-    int n,l,r; cin >>n;
+    int n,l,r,q; cin >>n>>q;
     SparseTable *st=new SparseTable;
     for(int i=0;i<n;i++)cin >> st->a[i];
     st->build(n);
-    int q;cin>>q;
+    int count=0;
     for(int i=0;i<q;i++){
         cin >>l>>r;
-        cout <<st->query(l,r)<<"\n";
+
+        l--;r-=2;
+        
+        if(st->query(l,r) <= st->a[l])count++;
     }
+    cout <<count;
     return 0;
 }
 
